58dca8cabeda77b7230c38ce37dd23665d678b48
Dhaanish K <freinds528@gmail.com>
2021-02-28 09:59:42 -0500

    final ver for cincyhacks
    Co-authored-by: StarNovain <StarNovain@users.noreply.github.com>
    Co-authored-by: Crown0fThornes <Crown0fThornes@users.noreply.github.com>

--- bot.py
+++ bot.py
@@ -1,26 +1,22 @@
-from asyncio.tasks import as_completed
-from asyncio.windows_events import INFINITE
 from discord.ext import tasks, commands
 import asyncio
 import speech # <---- imports speech.py
 import discord
 import time # < ---- apparently not used?
-import interval as rep
 import safe
 
-iD = 815383522251374613
+genID = safe.generalID
+hydraID = safe.hydraID
 
-# Bot checks whether user input has changed
 client = discord.Client() 
 
+
 @client.event
-async def p2D(text):
-    global loop
-    #await client.wait_until_ready
-    channel = client.get_channel(iD)
+async def p2D(text, ID):
+    channel = client.get_channel(ID)
 
     try:
-        await channel.send(text)                    ###ISSUE WITH bOut!!!!!!! bOut!!!!!!!
+        await channel.send(text)
     except Exception as e:
         print("----- " + "Error: {0}".format(e) + " -----")
 
@@ -31,97 +27,97 @@ s1 , s2 = "", ""
 
 @tasks.loop(seconds = 1)
 async def check(): #Checks to see if out has had any changes
-    print("Entered check()")
-    global s1, s2                         # may cause problems
+    # print("Entered check()")
+    global s1, s2                               # the change check vars
 
     bOut = speech.sent.lower()                  #Updates bOut with changes from speech.py
-    
-    s2 = bOut                                   #s2 declared to check change in sentences !!!DO NOT MOVE
 
-    if s1 != s2: #bOut != speech.sent.lower():
-        # Next step: output these commands to discord 
-        print("Entered If")
+    s2 = bOut                                   #s2 declared to check change in sent    ences !!!DO NOT MOVE
 
-        if ("goodnight" or "good night") in bOut: 
+    if s1 != s2:
+        if ("good night") in bOut: 
             wake = False
             speech.stop_listening(wait_for_stop=False)
             check.stop()
             value = "sleep"
-            try:
-                await p2D(bOut)
+            try:    
+                await p2D("Sleeping", genID)
             except Exception as e:
                 print("Error: {0}".format(e))
-        elif "test" in bOut:
-            print("Entered test")
-            await p2D("Testing... Testing...")
-        elif "party" in bOut:
-            print("Entered party")
-            await p2D("It's party time!")
-        elif "jump" in bOut:
-            print("entered jump")
-            await p2D("We're jumping super high!")
+        
+        elif ("goodnight") in bOut: 
+            wake = False
+            speech.stop_listening(wait_for_stop=False)
+            check.stop()
+            value = "sleep"
+            try:    
+                await p2D("Sleeping", genID)
+            except Exception as e:
+                print("Error: {0}".format(e))
+
+        elif ("hello buddy" in bOut):
+            # print("Entered test")
+            await p2D("Hey! How are you doing?", genID)
+
+        elif "weather" in bOut:
+            # print("Entered party")
+            await p2D("It's nice outside!", genID)
+        
+        elif "what do you do" in bOut:
+            # print("entered jump")
+            await p2D("I am a speech to text bot. I can recognize what you say, when you say it and push what you say to Discord!", genID)
+        
         elif "python" in bOut:
-            print("Entered python")
-            await p2D("Better than js")
+            # print("Entered python")
+            await p2D("Python: Better than js", genID)
+        
+        elif "javascript" in bOut:
+            # print("Entered python")
+            await p2D("Javascript: Better than py", genID)
+        
+        # play user inputted song with hyrda -- to be removed ?
+        elif "help" in bOut:
+            await p2D("I'm here to help! Just speak normally -- when I hear a keyword I will act accordingly!", genID)
+
+        # elif "lo-fi" in bOut:
+        #     await p2D("!clear 100", genID)
+    
     await asyncio.sleep(1)
                             
     s1 = bOut                                   #s1 declared to check change in text !!!DO NOT MOVE
 
+#Waits until bot is ready to start loop testing check()
 @check.before_loop
 async def preCheck():
     await client.wait_until_ready()
 
-# def stop():
-#     task.cancel()
-
+#Bot has successfully logged in and is ready to go!
 @client.event
 async def on_ready():
-    #global task, loop
-    #global interval
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="your voices"))
+
 
 # Bot reacts to user's message
 # 
 # @param message: discord reader/sender
-
 @client.event
 async def on_message(message): 
-    #print(message)
     if message.author == client.user:
         return
 
     if message.content.startswith('!hello'):
-        #Code
-        #await p2D("YOOOO")
-        #await check()
         await message.channel.send("Hello!")
-    elif message.content.startswith('!wake'):   #Wakes the bot up
-        # add line to not respond if already awake and vise versa 
+
+    elif message.content.startswith('!wake'):        #Wakes the bot up
         await message.channel.send('I have awoken!')
         wake = "True"
         speech.stop_listening(wait_for_stop=True)
-        # loop.run_forever()
-        # task = loop.create_task(check())
-    elif message.content.startswith('!sleep'):  #Turns the bot off
+
+    elif message.content.startswith('!sleep'):       #Turns the bot off
         await message.channel.send("I'm taking a nap")
         wake = "False"
         speech.stop_listening(wait_for_stop=False)
 
-print("do we get here")
-
 check.start()
 client.run(safe.token)
-#wake = input() # "True"
-print('[bot.py] bot is dead')
-#loop.close()
-
-# while True:
-#     if wake == True:
-        
-#         print("Iterating...")
-#     elif wake == False:
-#         interval.stop()
-#         print("Interval Stopping...")
-
-# elif message.content.startswith('!'):
-#         #Code
